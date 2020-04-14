15f7b7a40cc8cddf6f17f935fbcd8df5ccac4537
Kevin Harvey Jr <lordty@gmail.com>
2020-04-14 15:19:20 -0500

    Update Player Class

--- RPSLS/Player.cs
+++ RPSLS/Player.cs
@@ -7,9 +7,16 @@ namespace RPSLS
     class Player
     {
         //Variable (has a)
-
+        public string name;
+        public int score;
+        public int choice;
         //Constructor
-
+        public Player()
+        {
+            score = 0;
+        }
         //Method (can do)
+        
+
     }
 }
